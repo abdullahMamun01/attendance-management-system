4a8bc40719593942a5e2c7c38cbca8c7654de53a
unknown <abdullah.mamun.0110@gmail.com>
2023-09-19 23:36:57 +0600

    add js doc in student-attendance route

--- routes/student-attendance.js
+++ routes/student-attendance.js
@@ -4,8 +4,20 @@ const router = require("express").Router()
 
 // getStatus
 
-
+/** show available running status
+ * @function  getAttendanceStatus
+ * @method GET  
+ * @route '/status'
+ */
 router.get("/status" , getAttendanceStatus )
+
+/** attend in the attendance by id
+ * @function  getAttendance
+ * @method GET  
+ * @route '/:id'
+ * @param {string} id The ID of the attendance record to retrieve.
+ * 
+ */
 router.get("/:id", getAttendance)
 
 
